8e79212162dcfb57b1ee47ceb01e918c9a767a45
Anita Napodano <anita@MacBook-Pro-di-Anita.local>
2021-01-24 18:04:26 +0100

    clear data modificato

--- 01_cleardata.R
+++ 01_cleardata.R
@@ -1,13 +1,12 @@
 
 #variable happy_cntry comprising Italy, Latvia and Finland
-#dropped missing observations code 77(refusal), 88(don't know), 99)no anwer)
+#dropped missing observations code 77(refusal), 88(don't know), 99(no answer)
 happy_cntry <- filter(
   happy_data,
   cntry %in% c("IT", "FI"),
   !(happy==77 | happy==88 | happy==99)
 )
 View(happy_cntry)
-
 
 
 #variable happy_it comprising Italy 
