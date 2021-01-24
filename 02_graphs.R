5e3be55c3ae3cbd96b02bf36f42f64251de0fa5c
Anna Cernecca <annacernecca@gmail.com>
2021-01-24 22:05:17 +0100

    add a graph

--- 02_graphs.R
+++ 02_graphs.R
@@ -1,13 +1,27 @@
 #graph for the level of happiness in Italy
 barplot((table(happy_it$happy)),
-        col="red",
+        col="blue",
         xlab = "Level of happiness",
         ylab = "Frequency",
         main = "How happy are you?")
 
 #graph for the level of happiness in Finland
 barplot((table(happy_fi$happy)),
-        col="blue",
+        col="red",
         xlab = "Level of happiness",
         ylab = "Frequency",
         main = "How happy are you?")
+
+
+#graph for the level of happiness of both countries
+happy_cntry %>%
+  ggplot(aes(x = happy, fill = cntry)) +
+  geom_bar(alpha = 1, col = "black", binwidth = 10) +
+  scale_x_continuous(breaks = seq(0, 10, by = 1))+
+  scale_color_manual(values = c("red", "blue")) +
+  xlab("level of happiness") +
+  ylab("frequency") +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+
